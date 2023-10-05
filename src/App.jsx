298012f4c58c2c129076dd41b69e7f6ebae5feac
Renan Lucena <renan.segura.de.lucena@gmail.com>
2023-10-05 13:54:00 -0300

    adicionando o Normalize ao projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import styled from "styled-components"
+import EstilosGlobais from "./components/EstilosGlobais"
+
+const Fundo = styled.div` background: #5A57FF;
+width: 100%;
+min-height: 100vh;
+ `
 
 function App() {
  
 
   return (
-    <>
-      
-    </>
+   <Fundo>
+    <EstilosGlobais/>
+
+   </Fundo>
   )
 }
 
